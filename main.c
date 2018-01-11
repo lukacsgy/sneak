a596499e1199d45a400f0f92c02035c422e26d5c
György Lukács <georgelucasletters@gmail.com>
2018-01-11 20:23:33 +0100

    megcsinaltuk 1 D sneak-et!

--- main.c
+++ main.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <conio.h>
 
-int main()
-{
-    printf("Hello world!\n");
+/* Signal Handler for SIGINT */
+void sigint_handler(int sig_num) {
+    printf("\n User provided signal handler for Ctrl+C \n");
+    exit(0);
+}
+
+int main(void) {
+    signal(SIGINT, sigint_handler);
+
+    int i, k, k2, c = 0;
+    int szam = 0;
+
+    while(1) {
+        k = getch();
+        if (k = 224){
+            k2 = getch();
+            if(k2 == 72){
+                /*printf(" Felso kurzor!");*/
+            }
+            if(k2 == 75 && szam > 0){
+                /*printf(" Bal kurzor!");*/
+                szam--;
+                system("cls");
+                for(i = 0; i < szam; i++){
+                    printf("_");
+                }
+                printf("@\n");
+
+            }
+            if(k2 == 77 && szam < 19){
+                /*printf(" Jobb kurzor!");*/
+                szam++;
+                system("cls");
+                for(i = 0; i < szam; i++){
+                    printf("_");
+                }
+
+                printf("@\n");
+
+            }
+            if(k2 == 80){
+                /*printf(" Also kurzor!");*/
+
+            }
+        }
+    }
+
+
+
+
     return 0;
 }
